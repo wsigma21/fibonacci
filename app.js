ad160836ceecd9b9e1af239f6b8f37f04012704d
abe <raifo1991@yahoo.co.jp>
2020-05-24 16:14:38 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const number = process.argv[2] || 0;
+console.log(fib(number));
+
+/**
+ * フィボナッチ数列のうち、num番目を返す
+ * @param {Number} num 
+ * @returns {Number}
+ */
+function fib(num) {
+    let ans = 0;
+    if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else {
+        return fib(num - 1) + fib(num - 2)
+    }
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
